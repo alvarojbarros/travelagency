85faeec25fa2711abbaa272f800e9829e5f48dce
alvarojbarros <alvarojbarros@gmail.com>
2017-08-12 12:56:29 -0300

    Login y Logout de Usuarios

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
+from flask_login import LoginManager, login_required, login_user, logout_user,current_user
 app = Flask(__name__)
 from db.User import User
 from tools.dbtools import Session
@@ -9,13 +10,18 @@ app.config.update(
     SECRET_KEY = 'SECRET_KEY',
 )
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
 @app.route('/')
-#@login_required
+@login_required
 def home():
-    session = Session()
+    '''session = Session()
     session.expire_on_commit = False
 
-    '''new_user = User()
+    new_user = User()
     new_user.Email = '4@m.com'
     new_user.Name = 'Alvaro Jpse'
     new_user.Password = '123'
@@ -27,15 +33,49 @@ def home():
     except Exception as e:
         session.rollback()
         session.close()
-        return str(e) '''
+        return str(e)
+
     session = Session()
     records = session.query(User)
     l = []
     for record in records:
         l.append(record.Email)
-    return str(l)
+    return str(l) '''
+    return '<a href="/logout">LOGIUT</a>'
+
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        if username: username = username.replace(" ", "")
+        user = User.getUserIdByEmail(username)
+        password = request.form['password']
+        if username or password:
+            if not password or not username:
+                return render_template('login.html',error_msg='Debe Ingresar Usuario y Password',signUp=False)
+            if not user:
+                return render_template('login.html',error_msg='Usuario no Registrado',signUp=False)
+            if (user.Password == password):
+                if login_user(user):
+                    return redirect('/')
+        return render_template('login.html',error_msg='Datos Incorrectos',signIn=False)
+    else:
+        return render_template('login.html',signUp=False)
 
 
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return render_template('login.html',signUp=False)
+    #return Response('<p>Logged out</p>')
+
+# callback to reload the user object
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
 
 
 if __name__ == "__main__":
--- db/User.py
+++ db/User.py
@@ -2,7 +2,7 @@ from sqlalchemy import Table, Column, Integer, String, ForeignKey, Time, DateTim
 from flask_login import UserMixin
 from tools.Record import Record
 from sqlalchemy.ext.declarative import declarative_base
-from tools.dbtools import engine
+from tools.dbtools import engine,Session
 
 Base = declarative_base()
 
@@ -11,6 +11,25 @@ class User(Base,Record,UserMixin):
     Email = Column(String(50))
     Password = Column(String(20))
     Name = Column(String(40))
+
+    @classmethod
+    def getUserIdByEmail(cls,email):
+        session = Session()
+        record = session.query(cls).filter_by(Email=email).first()
+        if not record:
+            return
+        session.close()
+        return record
+
+    @classmethod
+    def get(cls,id):
+        session = Session()
+        user_data = session.query(cls).filter_by(id=id).first()
+        if not user_data:
+            return
+        session.close()
+        return user_data
+
 
 Index('Email', User.Email, unique=True)
 
